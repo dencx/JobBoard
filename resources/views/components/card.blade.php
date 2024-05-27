ec28e2b181f01105592724c205530d4478f403a6
Denys Bieliaiev <81828135+dencx@users.noreply.github.com>
2024-05-27 23:23:45 +0200

    add tag component & job info

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,4 @@
-<div {{ $attributes->merge(['class' => 'rounded-md border border-slate-300 bg-white p-4 shadow-sm'])}}>
+<article 
+    {{ $attributes->merge(['class' => 'rounded-md border border-slate-300 bg-white p-4 shadow-sm']) }}>
     {{ $slot }}
-</div>
+</article>
