5593c6d321bd1f65868b108553131ea119f59f83
Denys Bieliaiev <81828135+dencx@users.noreply.github.com>
2024-06-04 11:56:55 +0200

    add gradient background, style buttons & add alpine.js

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -1,13 +1,13 @@
 <div>
     <label for="{{ $name }}" class="mb-1 flex items-center">
         <input type="radio" name="{{ $name }}" value=""
-        @checked(!request('{{ $name }}'))/>
+        @checked(!request($name))/>
         <span class="ml-2">All</span>
     </label>
     @foreach ($optionsWithLabels as $label => $option)
     <label for="{{ $name }}" class="mb-1 flex items-center">
         <input type="radio" name="{{ $name }}" value="{{ $option }}" 
-        @checked(request('{{ $name }}') === $option)/>
+        @checked(request($name) === $option)/>
         <span class="ml-2">{{ $label }}</span>
     </label>
     @endforeach
