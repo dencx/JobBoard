9f12a87147c14291e6bf5300e228c63ff931276b
Denys Bieliaiev <81828135+dencx@users.noreply.github.com>
2024-06-08 23:55:05 +0200

    show other employer jobs on the job page

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -3,4 +3,24 @@
     <x-job-card :$job>
         <p class="text-sm text-slate-500 mb-4">{!! nl2br(e($job->description)) !!}</p>
     </x-job-card>
+    <x-card class="mb-4">
+        <h2 class="text-lg font-medium mb-4">
+            More {{ $job->employer->company_name }} Jobs
+        </h2>
+        <div class="text-sm text-slate-500">
+            @foreach ($job->employer->jobs as $otherJob)
+                <div class="mb-4 flex justify-between">
+                    <div>
+                        <div class="text-slate-700">
+                            <a href="{{ route('jobs.show', $otherJob) }}">{{ $otherJob->title }}</a>
+                        </div>
+                        <div class="text-xs">
+                            {{ $otherJob->created_at->diffForHumans() }}
+                        </div>
+                    </div>
+                    <div class="text-xs">${{ number_format($otherJob->salary) }}</div>
+                </div>
+            @endforeach
+        </div>
+    </x-card>
 </x-layout>
