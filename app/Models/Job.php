a92b01a005f898a9b395e56d923b01dbfdaf420c
Denys Bieliaiev <81828135+dencx@users.noreply.github.com>
2024-06-08 21:56:10 +0200

    searching by emloyer name

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
+use function Laravel\Prompts\search;
+
 class Job extends Model
 {
     use HasFactory;
@@ -24,7 +26,10 @@ class Job extends Model
         return $query->when($filters['search'] ?? null, function ($query, $search) {
             $query->where(function ($query) use ($search) {
                 $query->where('title', 'like', '%' . $search . '%')
-                    ->orWhere('description', 'like', '%' . $search . '%');
+                    ->orWhere('description', 'like', '%' . $search . '%')
+                    ->orWhereHas('employer', function($query) use ($search) {
+                        $query->where('company_name', 'like', '%'  . $search . '%');
+                    });
             });
         })->when($filters['min_salary'] ?? null, function ($query, $minSalary) {
             $query->where('salary', '>=', $minSalary);
