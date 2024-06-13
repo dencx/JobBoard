468dc453811eecc47b2767618dab550696b0fcdd
Denys Bieliaiev <81828135+dencx@users.noreply.github.com>
2024-06-13 23:18:58 +0200

    uploading files functionality

--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -9,12 +9,19 @@
         <h2 class="text-lg font-medium mb-4">
             Your Job Application
         </h2>
-        <form action="{{ route('job.application.store', $job) }}" method="POST">
+        <form action="{{ route('job.application.store', $job) }}" method="POST"
+        enctype="multipart/form-data">
             @csrf
             <div class="mb-4">
                 <label for="expected_salary" class="mb-2 block text-sm font-medium text-slate-900">Expected salary</label>
                 <x-text-input type="number" name="expected_salary"/>
             </div>
+
+            <div class="mb-4">
+                <label class="mb-2 block text-sm font-medium text-slate-900" for="">Upload CV</label>
+                <x-text-input type="file" name="cv"></x-text-input>
+            </div>
+
             <x-button class="w-full">Apply</x-button>
         </form>
     </x-card>
